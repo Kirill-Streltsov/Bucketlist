6275e6cf73dd427e9173be8e79d987708e7cd133
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-08-05 13:48:18 +0200

    Initial Commit

---
+++ Bucketlist/BucketlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketlistApp.swift
+//  Bucketlist
+//
+//  Created by Kirill Streltsov on 05.08.23.
+//
+
+import SwiftUI
+
+@main
+struct BucketlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
